4cce054a15c70fc4344ed8563882648787bff708
55019 <55019@etu.he2b.be>
2021-03-12 18:09:53 +0100

    Création de la classe Tile et de ses méthodes (Accesseur et getters)

--- src/main/java/model/Tile.java
+++ src/main/java/model/Tile.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 /**
  *
- * @author IanCo
+ * @author g55019 / Cotton Ian
+ * La classe tuile définit une tuile.
+ * Celle-ci possède un nombre entre 1 et 20 (compris)
+ * Méthode de cette classe : getValue (le getter) et un constructeur
+ * Pas d'accesseur car la valeur d'une tuile ne change jamais.
  */
+
 public class Tile {
-    
+    private Integer value;
+
+    /*
+    Constructeur, construit une tuile qui possèdent comme attribut une valeur entre 1 et 20 (compris)
+    @param Integer, nombre compris entre 1 et 20
+     */
+    public Tile(Integer value) {
+        if(value < 1 || value > 20){
+            throw new IllegalArgumentException("Valeur de la tuile incorrecte");
+        }
+        this.value = value;
+    }
+
+    /*
+    Getter
+    @return Integer, valeur de la tuile entre 1 & 20 (compris)
+     */
+    public Integer getValue() {
+        return value;
+    }
 }
