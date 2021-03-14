e2a5202f960479ae3f27332f560deee13073774e
55019 <55019@etu.he2b.be>
2021-03-14 15:54:32 +0100

    ajout des STATE de l'enum state et de sa javadoc

--- src/main/java/model/State.java
+++ src/main/java/model/State.java
@@ -1,5 +1,33 @@
 package model;
-
+/**
+ * @author g55019 / Cotton Ian
+ * Represents the state of the current game and the actions that can be performed at each state of the game
+ */
 public enum State {
+    /*
+    You can only use start() if the game is not started -> go to picktile state
+     */
+    NOT_STARTED,
 
+    /*
+        You can only use picktile() in this state -> go to placeTile state
+     */
+    PICK_TILE,
+
+    /*
+    You can only putTile() in this state after that action, there will be a checking to see if the game is over
+    or if the turn is over (turn end / game over state)
+     */
+    PLACE_TILE,
+
+    /*
+    It's the turn of another player to play and you go back to the picktile state
+     */
+    TURN_END,
+
+    /*
+    the game is over, you can only restart one (-> bring back to pick tile state)
+     */
+    GAME_OVER;
+    
 }
--- src/main/java/model/test.java
+++ src/main/java/model/test.java
@@ -1,33 +1,19 @@
 package model;
 
+
 public class test {
     public static void main(String[] args) {
         Board plateau = new Board();
-        Position pos = new Position(3,4);
 
-        System.out.println(plateau.isFull());
-        plateau.afficherBoard();
-        plateau.put(new Tile(5),new Position(0,0));
-        plateau.put(new Tile(5),new Position(0,1));
-        plateau.put(new Tile(5),new Position(0,2));
-        plateau.put(new Tile(5),new Position(0,3));
-        plateau.put(new Tile(5),new Position(1,0));
-        plateau.put(new Tile(5),new Position(1,1));
-        plateau.put(new Tile(5),new Position(1,2));
-        plateau.put(new Tile(5),new Position(1,3));
-        plateau.put(new Tile(5),new Position(2,0));
-        plateau.put(new Tile(5),new Position(2,1));
-        plateau.put(new Tile(5),new Position(2,2));
-        plateau.put(new Tile(5),new Position(2,3));
-        plateau.put(new Tile(5),new Position(3,0));
-        plateau.put(new Tile(5),new Position(3,1));
-        plateau.put(new Tile(5),new Position(3,2));
-        plateau.put(new Tile(5),new Position(3,3));
+
+
+        plateau.canBePut(new Tile(1), new Position(1,2));
+
+        System.out.println(plateau.canBePut(new Tile(10), new Position(1, 1)));
+
 
         System.out.println("---------------------------------");
         plateau.afficherBoard();
-
-        System.out.println(plateau.isFull());
 
     }
 
