f9792ea4f9291144d8710804b180e1bea92ce5cf
55019 <55019@etu.he2b.be>
2021-03-17 09:46:26 +0100

    modification of the javadoc

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -6,19 +6,16 @@ public interface View {
 
     /**
      * Displays a message at the start of the game: game name, author, version.
-     * @throws IllegalStateException if current state is not NOT_Started
      */
     void displayWelcome();
 
     /**
      * displays the state of the game, the current player and his board as well as the tile he must place
-     * @throws IllegalStateException if the current state is not PLACE_TILE
      */
     void displayGame();
 
     /**
      * displays the number of the winning player.
-     * @throws IllegalStateException if the current state is not GAME_OVER
      */
     void displayWinner();
 
@@ -31,7 +28,6 @@ public interface View {
     /**
      * asks the user to enter a row number and a column number and returns them as a position.
      * She makes sure that this position is valid.
-     * @throws IllegalStateException if the state is not PLACE_TILE
      * @return Position, a position in the board
      */
     Position askPosition();
