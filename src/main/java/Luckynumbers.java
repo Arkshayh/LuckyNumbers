05ab9e4574822f7d0d1f8b77795cf1cdbc60eaaa
55019 <55019@etu.he2b.be>
2021-03-22 12:47:51 +0100

    Added luckynumbers method

--- src/main/java/Luckynumbers.java
+++ src/main/java/Luckynumbers.java
@@ -1,16 +1,14 @@
-package model;
-
-
 import controller.Controller;
+import model.Game;
 import view.MyView;
-import view.View;
 
-public class test {
+/**
+ * Launch the game Luckynumbers
+ */
+public class Luckynumbers {
     public static void main(String[] args) {
         Game game = new Game();
         Controller controller = new Controller(game, new MyView(game));
         controller.play();
-
     }
-
 }
